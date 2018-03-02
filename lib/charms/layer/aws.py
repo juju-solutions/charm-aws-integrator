5636dbb82fd1d1d1065065fff9d04299d298c4a5
Cory Johns <johnsca@gmail.com>
2018-03-02 17:02:54 -0500

    Implement instance tagging, add missing region param

--- lib/charms/layer/aws.py
+++ lib/charms/layer/aws.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from configparser import ConfigParser
 from pathlib import Path
 
@@ -10,42 +11,52 @@ def update_credentials_file(access_key, secret_key):
     config = ConfigParser()
     if conf_file.exists():
         config.read(str(conf_file))
-    if 'default' not in config.sections():
-        config.add_section('default')
-    config['default']['aws_access_key_id'] = access_key
-    config['default']['aws_secret_key'] = secret_key
+    if 'juju' not in config.sections():
+        config.add_section('juju')
+    config['juju']['aws_access_key_id'] = access_key
+    config['juju']['aws_secret_access_key'] = secret_key
     with conf_file.open('w') as fp:
         os.fchmod(fp.fileno(), 0o600)
         config.write(fp)
 
 
-def tag_instance(instance_id, tags):
+def _aws(cmd, subcmd, *args, **kwargs):
+    cmd = ['aws', '--profile', 'juju', cmd, subcmd]
+    for key, value in kwargs.items():
+        cmd.extend(('--{}'.format(key.replace('_', '-')), str(value)))
+    cmd.extend(args)
+    subprocess.check_call(cmd)
+
+
+def tag_instance(instance_id, region, tags):
+    _aws('ec2', 'create-tags', '--resources', instance_id, '--region', region,
+         '--tags', *['Key={},Value={}'.format(key, value or '')
+                     for key, value in tags.items()])
+
+
+def tag_security_group(instance_id, region, tags):
     pass
 
 
-def tag_security_group(instance_id, tags):
+def tag_subnet(instance_id, region, tags):
     pass
 
 
-def tag_subnet(instance_id, tags):
+def enable_elb(instance_id, region):
     pass
 
 
-def enable_elb(instance_id):
+def enable_ebs(instance_id, region):
     pass
 
 
-def enable_ebs(instance_id):
+def enable_route53(instance_id, region):
     pass
 
 
-def enable_route53(instance_id):
+def enable_s3_read(instance_id, region):
     pass
 
 
-def enable_s3_read(instance_id):
-    pass
-
-
-def enable_s3_write(instance_id):
+def enable_s3_write(instance_id, region):
     pass
