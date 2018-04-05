71fee85411ead65cbfe941a9ad423f918646cb82
Cory Johns <johnsca@gmail.com>
2018-04-05 09:23:14 -0400

    Switch to new layer.foo.x convention

--- reactive/aws.py
+++ reactive/aws.py
@@ -7,8 +7,7 @@ from charms.reactive import (
     clear_flag,
 )
 
-from charms.layer import status
-from charms.layer import aws as charm_lib
+from charms import layer
 
 
 @when_any('config.changed.access-key',
@@ -19,64 +18,64 @@ def update_creds():
 
 @when_not('charm.aws.creds.set')
 def get_creds():
-    toggle_flag('charm.aws.creds.set', charm_lib.get_credentials())
+    toggle_flag('charm.aws.creds.set', layer.aws.get_credentials())
 
 
 @when_all('snap.installed.aws-cli',
           'charm.aws.creds.set')
 @when_not('endpoint.aws.requested')
 def no_requests():
-    status.maintenance('cleaning up unused aws entities')
+    layer.status.maintenance('cleaning up unused aws entities')
     aws = endpoint_from_flag('endpoint.aws.requested')
-    charm_lib.cleanup(aws.application_names)
-    status.active('ready')
+    layer.aws.cleanup(aws.application_names)
+    layer.status.active('ready')
 
 
 @when_all('snap.installed.aws-cli',
           'charm.aws.creds.set',
           'endpoint.aws.requested')
 def handle_requests():
-    status.maintenance('granting integration requests')
+    layer.status.maintenance('granting integration requests')
     aws = endpoint_from_flag('endpoint.aws.requested')
     for request in aws.requests:
         if request.instance_tags:
-            charm_lib.tag_instance(
+            layer.aws.tag_instance(
                 request.instance_id,
                 request.region,
                 request.instance_tags)
         if request.unit_security_group_tags:
-            charm_lib.tag_unit_security_group(
+            layer.aws.tag_unit_security_group(
                 request.instance_id,
                 request.region,
                 request.unit_security_group_tags)
         if request.instance_subnet_tags:
-            charm_lib.tag_instance_subnet(
+            layer.aws.tag_instance_subnet(
                 request.instance_id,
                 request.region,
                 request.instance_subnet_tags)
         if request.requested_elb:
-            charm_lib.enable_elb(
+            layer.aws.enable_elb(
                 request.application_name,
                 request.instance_id,
                 request.region)
         if request.requested_ebs:
-            charm_lib.enable_ebs(
+            layer.aws.enable_ebs(
                 request.application_name,
                 request.instance_id,
                 request.region)
         if request.requested_route53:
-            charm_lib.enable_route53(
+            layer.aws.enable_route53(
                 request.application_name,
                 request.instance_id,
                 request.region)
         if request.requested_s3_read:
-            charm_lib.enable_s3_read(
+            layer.aws.enable_s3_read(
                 request.application_name,
                 request.instance_id,
                 request.region,
                 request.s3_read_patterns)
         if request.requested_s3_write:
-            charm_lib.enable_s3_write(
+            layer.aws.enable_s3_write(
                 request.application_name,
                 request.instance_id,
                 request.region,
